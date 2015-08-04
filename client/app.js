76fdb97797ca37df8770f4617abf717952f99dc2
johndrkurtcom <john@drkurt.com>
2015-08-04 10:25:15 -0700

    basic title and timetable are working and showing up on screen

--- client/app.js
+++ client/app.js
@@ -1,2 +1,42 @@
-angular.module('sched', ['sched.title', 'sched.timeTable']);
+var app = angular.module('sched', ['sched.title']);
 
+
+
+app.controller('appController', function ($scope) {
+  $scope.title = 'M3AAWG';
+});
+
+app.controller('timeTableController', function ($scope) {
+  $scope.table = {
+    times: [],
+    height: 0,
+  };
+
+  $scope.buildTimeArray = function(start, end){
+    //assumes start is am and end is pm i.e. 800, 1930
+    var results = [];
+    var index = start;
+    while(index <= end){
+      if(index%50 === 0){
+        results.push(index.toString());
+        index += 30;
+      }else{
+        results.push(index.toString());
+        index += 70;
+      }
+    }
+    return results;
+  };
+
+  $scope.table.times = $scope.buildTimeArray(800, 1930);
+  $scope.table.height = (100 / $scope.table.times.length);
+
+});
+
+// .config(function($routeProvider){
+//   $routeProvider
+//   .when('/', {
+//     templateUrl: 'views/title.html',
+//     controller: 'titleController'
+//   })
+// })
